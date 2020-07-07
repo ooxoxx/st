e9db71064a5b9baa20d47b7b21329be8bb944bde
leo <ooxoxx@live.cn>
2020-07-08 05:55:18 +0800

    color scheme changed.

--- config.h
+++ config.h
@@ -92,42 +92,29 @@ unsigned int tabspaces = 8;
 /* bg opacity */
 float alpha = 0.85;
 
-/* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-  /* 8 normal colors */
-  [0] = "#000000", /* black   */
-  [1] = "#ff5555", /* red     */
-  [2] = "#50fa7b", /* green   */
-  [3] = "#f1fa8c", /* yellow  */
-  [4] = "#bd93f9", /* blue    */
-  [5] = "#ff79c6", /* magenta */
-  [6] = "#8be9fd", /* cyan    */
-  [7] = "#bbbbbb", /* white   */
-
-  /* 8 bright colors */
-  [8]  = "#44475a", /* black   */
-  [9]  = "#ff5555", /* red     */
-  [10] = "#50fa7b", /* green   */
-  [11] = "#f1fa8c", /* yellow  */
-  [12] = "#bd93f9", /* blue    */
-  [13] = "#ff79c6", /* magenta */
-  [14] = "#8be9fd", /* cyan    */
-  [15] = "#ffffff", /* white   */
-
-  /* special colors */
-  [256] = "#282a36", /* background */
-  [257] = "#f8f8f2", /* foreground */
+  "#1e1e1e", /* base00 */
+  "#cf6a4c", /* base08 */
+  "#8f9d6a", /* base0B */
+  "#f9ee98", /* base0A */
+  "#7587a6", /* base0D */
+  "#9b859d", /* base0E */
+  "#afc4db", /* base0C */
+  "#a7a7a7", /* base05 */
+  "#5f5a60", /* base03 */
+  "#cda869", /* base09 */
+  "#323537", /* base01 */
+  "#464b50", /* base02 */
+  "#838184", /* base04 */
+  "#c3c3c3", /* base06 */
+  "#9b703f", /* base0F */
+  "#ffffff", /* base07 */
 };
 
-
-/*
- * Default colors (colorname index)
- * foreground, background, cursor, reverse cursor
- */
-unsigned int defaultfg = 257;
-unsigned int defaultbg = 256;
-static unsigned int defaultcs = 257;
-static unsigned int defaultrcs = 257;
+unsigned int defaultfg = 7;
+unsigned int defaultbg = 0;
+static unsigned int defaultcs = 13;
+static unsigned int defaultrcs = 0;
 
 /*
  * Colors used, when the specific fg == defaultfg. So in reverse mode this
